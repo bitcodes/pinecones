f9dfa7c2eec1b655674d0849d7da42fd7e552eb8
sergey zadvorniy <sergey.zadvorniy@gmail.com>
2013-04-30 10:51:12 +0300

    mobile device chechout page + collapsed tabs fix

--- sites/all/themes/nickandersonart/js/checkout.js
+++ sites/all/themes/nickandersonart/js/checkout.js
@@ -6,22 +6,39 @@
     $(document).ready(function() {
         //Make collapsible cart pane
         
-        $("#cart-pane").addClass("collapsible collapse-processed");
-        $("#cart-pane legend").css('display', 'none').click(function(){
-            $("#cart-pane").toggleClass("collapsed");
-            $("#cart-pane legend").css('display', 'none');
-            
-        });
+        var win_w = $(window).width(); //check device width
         
-        $("#delivery-pane").addClass("collapsed");
-        $("#cart_next").click(function(){
-            $("#cart-pane").addClass("collapsed");
-            $("#delivery-pane").removeClass("collapsed");
-            $("#delivery-pane .fieldset-wrapper").css('display', 'block');
-            $(".ttl_sb").css('display', 'none');
-            $("#cart-pane legend").css('display', 'block');
-            $("#cart-pane legend").css('cursor', 'pointer');
-        });
+        //if mobile device
+        if(win_w < 470){
+            //do something code for mobile device
+                         
+            $('#uc-cart-checkout-form').find('.form-submit').click(function(){
+                var divToScroll = $(this).parents('fieldset');
+                $(this).parents('fieldset').next().addClass('test');
+                $('html,body').animate({scrollTop: $(divToScroll).offset().top}, 500);
+            });
+           
+        }
+        
+        //if desctop browser 
+        
+            $("#cart-pane").addClass("collapsible collapse-processed");
+            $("#cart-pane legend").css('display', 'none').click(function(){
+                $("#cart-pane").toggleClass("collapsed");
+                $("#cart-pane legend").css('display', 'none');
+
+            });
+
+            $("#delivery-pane").addClass("collapsed");
+            $("#cart_next").click(function(){
+                $('html,body').animate({scrollTop: $("#delivery-pane").offset().top}, 500);
+                $("#cart-pane").addClass("collapsed");
+                $("#delivery-pane").removeClass("collapsed");
+                $("#delivery-pane .fieldset-wrapper").css('display', 'block');
+                $(".ttl_sb").css('display', 'none');
+                $("#cart-pane legend").css('display', 'block');
+                $("#cart-pane legend").css('cursor', 'pointer');
+            });
         
         //Make quotes-pane collapsed
         $("#quotes-pane .fieldset-wrapper").css('display', 'none');
